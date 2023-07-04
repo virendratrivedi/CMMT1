f1fdf090478ce6687304c235c42a12cf81491d87
Arpit Trivedi <67951209+virendratrivedi@users.noreply.github.com>
2023-07-04 11:36:04 +0530

    Core Modules Mock Test
    Core Modules Mock Test

--- CMMT1.py
+++ CMMT1.py
@@ -87,6 +87,38 @@ mean_value = calculate_mean(data)
 print("Mean:", mean_value)
 
 
+# ## Write a function called ‘perform_hypothesis_test’ that takes two lists of numbers as input, representing two samples. The function should perform a two-sample t-test and return the p-value. Use the ‘scipy.stats’ module in Python to calculate the t-test and p-value.
+# 
+# Example:
+# 
+# from scipy import stats
+# 
+# def perform_hypothesis_test(sample1, sample2):
+#     t_statistic, p_value = stats.ttest_ind(sample1, sample2)
+#     return p_value
+# 
+# sample1 = [5, 10, 15, 20, 25]
+# sample2 = [10, 20, 30, 40, 50]
+# p_value = perform_hypothesis_test(sample1, sample2)
+# print("P-value:", p_value)
+# 
+# Output:
+# P-value: 0.1064706396450037
+
+# In[5]:
+
+
+from scipy import stats
+
+def perform_hypothesis_test(sample1, sample2):
+    t_statistic, p_value = stats.ttest_ind(sample1, sample2)
+    return p_value
+sample1 = [5, 10, 15, 20, 25]
+sample2 = [10, 20, 30, 40, 50]
+p_value = perform_hypothesis_test(sample1, sample2)
+print("P-value:", p_value)
+
+
 # In[ ]:
 
 
